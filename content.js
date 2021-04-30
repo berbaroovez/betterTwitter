9c389612077e9383ac7993a466b41ade19798b82
berbaroovez <carterolson3@gmail.com>
2021-04-30 15:03:48 -0500

    testing loops

--- content.js
+++ content.js
@@ -1,6 +1,42 @@
 console.log("YOOOOOOOOO");
+var testArray = [
+  {
+    id: "5f4ae8653769246c03209da2",
+    code: "stanzTasty",
+    imageType: "gif",
+    userId: "5573d291c7a34e186b3222bb",
+  },
+  {
+    id: "602600b2d47a0b2db8d1a99a",
+    code: "stanzJAM",
+    imageType: "gif",
+    userId: "5573d291c7a34e186b3222bb",
+  },
+  {
+    id: "5fc86066e22688461fed94b7",
+    code: "OMEGALAW",
+    imageType: "png",
+    userId: "5573d291c7a34e186b3222bb",
+  },
+  {
+    id: "5ada077451d4120ea3918426",
+    code: "blobDance",
+    imageType: "gif",
+    userId: "5573d291c7a34e186b3222bb",
+  },
+  {
+    id: "5c04c335693c6324ee6a23b2",
+    code: "peepoPog",
+    imageType: "gif",
+    userId: "5573d291c7a34e186b3222bb",
+  },
+];
+// var targetNode = document.querySelector('main[role="main"]');
+var targetNode = document.querySelector(
+  'div[aria-label="Timeline: Your Home Timeline"]'
+);
+var secondNode = document.querySelector('div[dir="auto"]');
 
-var targetNode = document.querySelector('main[role="main"]');
 main();
 async function main() {
   var twitchUserId = await validTwitchUserCheck("stanz");
@@ -11,12 +47,7 @@ async function main() {
 
   var callback = function (mutationsList) {
     for (var mutation of mutationsList) {
-      //   if (bttvEmoteList.length > 0) {
-      //     modifyDOM(bttvEmoteList);
-      //   } else {
-      //     console.log("No emotes available");
-      //   }
-      modifyDOM();
+      modifyDOMTest();
     }
   };
   var observer = new MutationObserver(callback);
@@ -55,7 +86,7 @@ async function main() {
   }
 }
 
-function modifyDOMTest(emoteList) {
+function modifyDOMTest() {
   //You can play with your DOM here or check URL against your regex
 
   const tweetTexts = document.querySelectorAll("span");
@@ -64,27 +95,23 @@ function modifyDOMTest(emoteList) {
   // img.src = "https://cdn.betterttv.net/emote/5c04c335693c6324ee6a23b2/3x";
 
   for (let i = 0; i < tweetTexts.length; i++) {
-    //   for (emote in emoteList) {
-    if (tweetTexts[i].innerHTML.includes("peepoPog")) {
-      tweetTexts[i].innerHTML = tweetTexts[i].innerHTML.replace(
-        "peepoPog",
-        `<img style="width:25px;height:25px;" src="https://cdn.betterttv.net/emote/5c04c335693c6324ee6a23b2/3x">`
-      );
+    for (var a = 0; a < testArray.length; a++) {
+      if (tweetTexts[i].innerHTML.includes(testArray[a].code)) {
+        tweetTexts[i].innerHTML = tweetTexts[i].innerHTML.replace(
+          testArray[a].code,
+          `<img style="width:25px;height:25px;" src="https://cdn.betterttv.net/emote/${testArray[a].id}/3x">`
+        );
+      }
     }
-    if (tweetTexts[i].innerHTML.includes("blobDance")) {
-      tweetTexts[i].innerHTML = tweetTexts[i].innerHTML.replace(
-        "blobDance",
-        `<img style="width:25px;height:25px;" src="https://cdn.betterttv.net/emote/5ada077451d4120ea3918426/3x">`
-      );
-    }
-    //   }
   }
 }
 
 async function checkForNode() {
   while (!targetNode) {
     await new Promise((r) => setTimeout(r, 2000));
-    targetNode = document.querySelector('main[role="main"]');
+    targetNode = document.querySelector(
+      'div[aria-label="Timeline: Your Home Timeline"]'
+    );
     console.log("inside script:", targetNode);
   }
 
